5b17dde83b536cd7059fe2ab8f7aac435507e0a3
Taha Shaikh <tahashaikh@Mac.lan>
2026-02-10 18:12:17 +0530

    Added chapter9

---
+++ chapter9/01_class.py
@@ -0,0 +1,8 @@
+class Employee:
+    language = "py" # This is a class attribute
+    salray = 150000
+
+Taha = Employee()
+Taha.name = "Taha Shaikh" #This is a instance attribute
+print(Taha.name, Taha.language, Taha.salray)  
+
